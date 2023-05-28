36ee0bca88e83634779892c83cc1c17dd06b3546
chiadi <lcim1104@gmail.com>
2023-05-28 03:19:15 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import React from "react"
+import Die from "./Die"
+import {nanoid} from "nanoid"
+
+export default function App() {
+
+    const [dice, setDice] = React.useState(allNewDice())
+    
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid()
+            })
+        }
+        return newDice
+    }
+    
+    function rollDice() {
+        setDice(allNewDice())
+    }
+    
+/**
+ * Challenge: Update the `holdDice` function to flip
+ * the `isHeld` property on the object in the array
+ * that was clicked, based on the `id` prop passed
+ * into the function.
+ * 
+ * Hint: as usual, there's > 1 way to accomplish this.
+ * I'll be using `dice.map()` and checking for the `id`
+ * of the die to determine which one to flip `isHeld` on,
+ * but you can do whichever way makes the most sense to you.
+ */
+    function holdDice(id) {
+        setDice(oldDice => oldDice.map(die => {
+            return die.id === id ? 
+                {...die, isHeld: !die.isHeld} :
+                die
+        }))
+    }
+    
+    const diceElements = dice.map(die => (
+        <Die 
+            key={die.id} 
+            value={die.value} 
+            isHeld={die.isHeld} 
+            holdDice={() => holdDice(die.id)}
+        />
+    ))
+    
+    return (
+        <main>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+        </main>
+    )
+}
